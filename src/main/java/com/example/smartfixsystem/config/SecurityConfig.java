a3a8479e28c22b57db0ec9ce82cacf482ff51c45
fatis1403 <fatishseine@gmail.com>
2026-06-14 14:14:07 +0100

    add spring security login and improve demande management

--- src/main/java/com/example/smartfixsystem/config/SecurityConfig.java
+++ src/main/java/com/example/smartfixsystem/config/SecurityConfig.java
@@ -2,20 +2,39 @@ package com.example.smartfixsystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain security(HttpSecurity http) throws Exception {
+    public UserDetailsManager users() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("SmartFix@2026")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
 
         return http.build();
     }
